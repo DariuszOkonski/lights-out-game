a47063dcfa312600e24c5ccde139ea07c83e940f
Dariusz Okonski <darek200180@gmail.com>
2019-12-17 18:59:40 +0000

    Updates

---
+++ precache-manifest.8881ce93b548faabc7c7b0e0e6c7a1f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4f42c347412b793e214",
+    "url": "/lights-out-game/static/css/main.866d10b0.chunk.css"
+  },
+  {
+    "revision": "d4f42c347412b793e214",
+    "url": "/lights-out-game/static/js/main.d4f42c34.chunk.js"
+  },
+  {
+    "revision": "cd22fa7af0c3eaf7956e",
+    "url": "/lights-out-game/static/js/1.cd22fa7a.chunk.js"
+  },
+  {
+    "revision": "c21eb99d7eac6d163be2",
+    "url": "/lights-out-game/static/js/runtime~main.c21eb99d.js"
+  },
+  {
+    "revision": "c294433c2f6d6eaea08c57e4dcace889",
+    "url": "/lights-out-game/index.html"
+  }
+];
